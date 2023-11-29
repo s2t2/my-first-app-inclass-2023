4e86e1a252d61dac249a7da1f34aa8f5562346b3
Michael Rossetti <s2t2@users.noreply.github.com>
2023-11-29 18:02:52 -0500

    Web Application (#7)
    * Setup flask routing
    
    * Return example JSON (our own API)
    
    * Render HTML files
    
    * Shared layout
    
    * Bootstrap styles
    
    * Recap, carousel
    
    * Unemployment Report
    
    with flash messaging
    
    * Stocks functionality
    
    * stock dashboard
    
    * Configure server

--- README.md
+++ README.md
@@ -51,12 +51,39 @@ Run the unemployment report:
 python -m app.unemployment
 ```
 
+Run the stocks report:
+
+```sh
+python -m app.stocks
+```
+
 Send an example email:
 
 
 ```sh
 python app/email_service.py
 ```
+
+
+
+### Web App
+
+Run the web app (then view in the browser at http://localhost:5000/):
+
+```sh
+# Mac OS:
+FLASK_APP=web_app flask run
+
+# Windows OS:
+# ... if `export` doesn't work for you, try `set` instead
+# ... or set FLASK_APP variable via ".env" file
+export FLASK_APP=web_app
+flask run
+```
+
+
+
+
 
 
 ## Testing
--- app/unemployment.py
+++ app/unemployment.py
@@ -1,15 +1,14 @@
 
 # IMPORTS AT THE TOP
 
-import os
 import json
 from pprint import pprint
 from statistics import mean
 
-from dotenv import load_dotenv
 import requests
 from plotly.express import line
 
+from app.alpha import API_KEY
 from app.email_service import send_email
 
 print("BACK IN UNEMPLOYMENT FILE")
@@ -17,15 +16,24 @@ print("BACK IN UNEMPLOYMENT FILE")
 
 # ENVIRONMENT VARIABLES AND CONSTANTS
 
-load_dotenv() # go look in the .env file for any env vars
 
-API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
 #breakpoint()
 
 #quit()
 
 # FUNCTIONS
+
+def format_pct(my_number):
+    """
+    Formats a percentage number like 3.6555554 as percent, rounded to two decimal places.
+
+    Param my_number (float) like 3.6555554
+
+    Returns (str) like '3.66%'
+    """
+    return f"{my_number:.2f}%"
+
 
 def fetch_data():
 
